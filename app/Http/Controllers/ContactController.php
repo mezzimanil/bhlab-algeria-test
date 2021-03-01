925f2a96c44b3b99d76b9b7ec61740277e78bddb
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-01 17:52:50 +0100

    controller

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Mail;
+use App\Mail\ContactMail;
+use App\Rules\PhoneNumber;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('contact');
+    }
+   
+    public function sendEmail(Request $request)
+    {
+        $details = [
+           'name'=>$request->name,
+           'email'=>$request->email,
+           'phone'=>$request->phone,
+           'message'=>$request->message
+        ];
+
+        Mail::to('mmwikoo@gmail.com')->send(new ContactMail($details));
+    
+       return back()->with('message_sent','Votre message a été envoyer');    
+    }
+
+}
