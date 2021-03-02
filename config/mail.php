9d0925e317825e6fef2660564708811876fab666
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-02 15:08:33 +0100

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -43,7 +43,7 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
-            'authentication' => 'plain',
+           
         ],
 
         'ses' => [
