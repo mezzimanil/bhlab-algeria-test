fa55b7a0d2441ef319a501b6660b7ccf38f3528f
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-03 12:22:35 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -135,53 +135,32 @@ body {
 
 	
 	
-				<div id="myCarousel" class="carousel slide" data-ride="carousel">
-					<ol class="carousel-indicators">
-					  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-					  <li data-target="#myCarousel" data-slide-to="1"></li>
-					  <li data-target="#myCarousel" data-slide-to="2"></li>
-					</ol>
-					<div class="carousel-inner">
-					  <div class="carousel-item active">
-						<img class="first-slide" src="{{ asset('image/image3.jpg') }}" alt="First slide">
-						<div class="container">
-						  <div class="carousel-caption text-left">
-							<h1>Example headline.</h1>
-							<p style="color: white">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-							<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-						  </div>
-						</div>
-					  </div>
-					  <div class="carousel-item">
-						<img class="second-slide" src="{{ asset('image/skyscraper2.jpg') }}" alt="Second slide">
-						<div class="container">
-						  <div class="carousel-caption">
-							<h1>Another example headline.</h1>
-							<p style="color: white">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-							<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-						  </div>
-						</div>
-					  </div>
-					  <div class="carousel-item">
-						<img class="third-slide" src="{{ asset('image/wizard-city2.jpg') }}" alt="Third slide">
-						<div class="container">
-						  <div class="carousel-caption text-right">
-							<h1>One more for good measure.</h1>
-							<p style="color: white">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-							<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-						  </div>
-						</div>
-					  </div>
-					</div>
-					<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-					  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					  <span class="sr-only">Previous</span>
-					</a>
-					<a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-					  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-					  <span class="sr-only">Next</span>
-					</a>
-				  </div>
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+		  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+		  <div class="carousel-item active">
+			<img class="d-block w-100" src="..." alt="First slide">
+		  </div>
+		  <div class="carousel-item">
+			<img class="d-block w-100" src="..." alt="Second slide">
+		  </div>
+		  <div class="carousel-item">
+			<img class="d-block w-100" src="..." alt="Third slide">
+		  </div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+		  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		  <span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+		  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		  <span class="sr-only">Next</span>
+		</a>
+	  </div>
 			
 			 
 
