da4aa8b5e086faf8aee01ac4d89c8378a4f481e1
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-04 18:44:50 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,15 +66,9 @@
 </head>
 <body>
     <div id="app">
-      <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light ">
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-light ">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                BH LAB
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
+            
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
