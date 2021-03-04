baa2f730abf92128ced42a6bb0c82adbc37e62df
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-04 17:47:19 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
 <body>
     <div id="app">
         
-        <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light border-bottom border-dark">
+        <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light border-bottom border-success">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     BH LAB
