57bd4df7403d0668b02f22313cae3b4d1a5f21c8
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-04 18:28:19 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,14 +66,8 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-sm fixed-top  bg-info">
-          <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
-            <li class="nav-item">
-              <a class="nav-link"  href="#">{{ __('About us') }}</a>
-          </li>
-          </ul>
-        </nav>
+        
+        
         <nav class="navbar  navbar-light fixed-top bg-light border-bottom border-success">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
