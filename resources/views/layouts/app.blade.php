c7c54d34cda2eb17dfee50ef712b759dd5d80299
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-04 18:42:50 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,39 +66,59 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expend-md bg-info navbar-dark fixed-top">
-          <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
-            <li class="nav-item">
-              <a class="nav-link"  href="#">{{ __('About us') }}</a>
-          </li>
-           
-          
-        <li class="nav-item" >
-          <a class="nav-link"   href="#">{{ __('Produit') }}</a>
-      </li>
-            <li class="nav-item" >
-              <a class="nav-link"   href="{{ route('fournisseur') }}">{{ __('fournisseurs') }}</a>
-          </li>
+      <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light border-bottom border-success">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                BH LAB
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-         
-
-          
-          <li class="nav-item" >
-            <a class="nav-link"   href="#">{{ __('Ssdrrr') }}</a>
-        </li>
-
-       
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                    
 
 
-      <li class="nav-item">
-          <a class="nav-link"  href="#">{{ __('Support') }}</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link"  href="{{ route('contact') }}">{{ __('contacter-nous') }}</a>
-    </li>
-        </ul>
-        </nav>
+                    
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    <li class="nav-item">
+                      <a class="nav-link"  href="#">{{ __('About us') }}</a>
+                  </li>
+                   
+                  
+                <li class="nav-item" >
+                  <a class="nav-link"   href="#">{{ __('Produit') }}</a>
+              </li>
+                    <li class="nav-item" >
+                      <a class="nav-link"   href="{{ route('fournisseur') }}">{{ __('fournisseurs') }}</a>
+                  </li>
+
+                 
+
+                  
+                  <li class="nav-item" >
+                    <a class="nav-link"   href="#">{{ __('Ssdrrr') }}</a>
+                </li>
+
+               
+
+
+              <li class="nav-item">
+                  <a class="nav-link"  href="#">{{ __('Support') }}</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link"  href="{{ route('contact') }}">{{ __('contacter-nous') }}</a>
+            </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
         <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light border-bottom border-success">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
