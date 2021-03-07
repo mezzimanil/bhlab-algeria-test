3f8b4bfce217675f873d60c9a31cb79ca2b51d67
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-07 14:37:43 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,7 +101,16 @@
                        
                       
                     <li class="nav-item" >
-                      <a class="nav-link"   href="#">{{ __('Produit') }}</a>
+                      <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"  href="#">{{ __('Produit') }}</a>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li class="dropdown-header">Nav header</li>
+                        <li><a href="#">Separated link</a></li>
+                        <li><a href="#">One more separated link</a></li>
+                      </ul>
                   </li>
                         <li class="nav-item" >
                           <a class="nav-link"   href="{{ route('fournisseur') }}">{{ __('fournisseurs') }}</a>
