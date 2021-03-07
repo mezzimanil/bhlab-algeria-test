0c037026bd8080d1353296c887c5230ffb58b396
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-07 16:14:28 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,16 +109,46 @@
 
                      
 
-                      <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <a class="dropdown-item" href="#">Action</a>
-                          <a class="dropdown-item" href="#">Another action</a>
-                          <div class="dropdown-divider"></div>
-                          <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
+                      <li class="nav-item">
+                        <a href="#" class="nav-link dropdown-toggle">Dropdown</a>
+                        <ul class="dropdown-menu">
+                          <li class="nav-item">
+                            <a href="#" class="nav-link dropdown-toggle">Submenu 1</a>
+                            <ul class="dropdown-menu">
+                              <li class="nav-item">
+                                <a href="#" class="nav-link">Item 1</a>
+                              </li>
+                              <li class="nav-item">
+                                <a href="#" class="nav-link">Item 2</a>
+                              </li>
+                            </ul>
+                          </li>
+                          <li class="nav-item">
+                            <a href="#" class="nav-link dropdown-toggle">Submenu 2</a>
+                            <ul class="dropdown-menu">
+                              <li class="nav-item">
+                                <a href="#" class="nav-link">Item 1</a>
+                              </li>
+                              <li class="nav-item">
+                                <a href="#" class="nav-link">Item 2</a>
+                              </li>
+                              <li class="nav-item">
+                                <a href="#" class="nav-link dropdown-toggle">Submenu 3</a>
+                                <ul class="dropdown-menu">
+                                  <li class="nav-item">
+                                    <a href="#" class="nav-link">Item 1</a>
+                                  </li>
+                                  <li class="nav-item">
+                                    <a href="#" class="nav-link">Item 2</a>
+                                  </li>
+                                  <li class="nav-item">
+                                    <a href="#" class="nav-link">Item 3</a>
+                                  </li>
+                                </ul>
+                              </li>
+                            </ul>
+                          </li>
+                        </ul>
                       </li>
                       
                    
