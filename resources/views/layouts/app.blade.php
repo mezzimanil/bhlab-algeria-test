7969a373903f4cb9cf7f8248769be95384f741b9
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-08 16:21:50 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,61 +74,41 @@
     <div id="app">
    
 
-      <nav class="navbar navbar-light bg-light navbar-expand-lg" id="myNavbar">
- 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
-         <span class="navbar-toggler-icon"></span>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light  fixed-top">
+        <div class="container">
+          <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('image/lol.png') }}">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav">
+          <span class="navbar-toggler-icon"></span>
         </button>
+        <div class="collapse navbar-collapse" id="main_nav">
         
-        <a href="#" class="navbar-brand">Navbar Brand</a>
-        <div class="collapse navbar-collapse" id="mainNav">
-         <ul class="navbar-nav ml-auto nav-fill">
-          
-          <li class="nav-item px-4">
-           <a href="#" class="nav-link">Home <span class="sr-only">(current)</span></a>
+       
+        
+        <ul class="navbar-nav ml-auto">
+          <ul class="navbar-nav">
+           
+              
+            <li class="nav-item"> <a class="nav-link" href="#">Menu item </a> </li>
+            <li class="nav-item"><a class="nav-link" href="#"> About </a></li>
+            <li class="nav-item"><a class="nav-link" href="#"> Services </a></li>
+          </ul>
+          <li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
+          <li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
+          <li class="nav-item dropdown">
+            <a class="nav-link  dropdown-toggle" href="#" data-toggle="dropdown"> Dropdown right </a>
+            <ul class="dropdown-menu dropdown-menu-right">
+            <li><a class="dropdown-item" href="#"> Submenu item 1</a></li>
+            <li><a class="dropdown-item" href="#"> Submenu item 2 </a></li>
+            </ul>
           </li>
-          
-          <li class="nav-item px-4 dropdown">
-           <a class="nav-link dropdown-toggle" href="#" id="servicesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Services</a>
-           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="servicesDropdown">
-            <a class="dropdown-item" href="#">What we do</a>
-            <a class="dropdown-item" href="#">How we fit your needs</a>
-            <div class="dropdown-divider"></div>
-            <div class="d-md-flex align-items-start justify-content-start">
-             <div>   
-              <div class="dropdown-header">Development</div>
-              <a class="dropdown-item" href="#">Bespoke software</a>
-              <a class="dropdown-item" href="#">Mobile apps</a>
-              <a class="dropdown-item" href="#">Websites</a>
-             </div>
-             <div>
-              <div class="dropdown-header">Professional Services</div>
-              <a class="dropdown-item" href="#">Project rescue</a>
-              <a class="dropdown-item" href="#">Source code recovery</a>
-              <a class="dropdown-item" href="#">Application support &amp; maintenance</a>
-             </div>
-             <div>
-              <div class="dropdown-header">Fixed Price Services</div>
-              <a class="dropdown-item" href="#">Add cookie consent</a>
-              <a class="dropdown-item" href="#">Add captcha</a>
-              <a class="dropdown-item" href="#">Add core data</a>
-              <a class="dropdown-item" href="#">Custom error pages</a>
-              <a class="dropdown-item" href="#">Contact form creation</a>
-              <a class="dropdown-item" href="#">Automated backups</a>
-              <a class="dropdown-item" href="#">Image to HTML</a>
-             </div>
-            </div>
-           </div>
-          </li>
-          <li class="nav-item px-4">
-           <a href="#" class="nav-link">Contact</a>
-          </li>
-          <li class="nav-item px-4">
-           <a href="#" class="nav-link">Blog</a>
-          </li>
-         </ul>
+        
+        </ul>
+        
+        </div> <!-- navbar-collapse.// -->
         </div>
-       </nav>
+        </nav>
        
               
              
