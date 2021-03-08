33220e146c6e322cca7753f4a49df60015a64bf8
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-08 12:37:17 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -99,63 +99,64 @@
         </button>
         <div class="collapse navbar-collapse" id="main_nav">
         
-        <ul class="navbar-nav">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"> Mega submenu </a>
-            <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#"> Dropdown item 1 </a></li>
-            <li><a class="dropdown-item" href="#"> Dropdown item 2 </a></li>
-            <li><a class="dropdown-item" href="#"> Dropdown item 3 </a></li>
-            <li class="has-submenu">
-               <a class="dropdown-item dropdown-toggle" href="#"> Dropdown item 4 </a>
-               <div class="megasubmenu dropdown-menu">
-               <div class="row">
-                  <div class="col-6">
-                      <h6 class="title">Title Menu One</h6>
-                      <ul class="list-unstyled">
-                        <li><a href="#">Custom Menu</a></li>
-                        <li><a href="#">Custom Menu</a></li>
-                        <li><a href="#">Custom Menu</a></li>
-                        <li><a href="#">Custom Menu</a></li>
-                        <li><a href="#">Custom Menu</a></li>
-                      </ul>
-                  </div><!-- end col-3 -->
-                  <div class="col-6">
-                    <h6 class="title">Title Menu Two</h6>
-                      <ul class="list-unstyled">
-                        <li><a href="#">Custom Menu</a></li>
-                        <li><a href="#">Custom Menu</a></li>
-                        <li><a href="#">Custom Menu</a></li>
-                        <li><a href="#">Custom Menu</a></li>
-                      </ul>
-                  </div><!-- end col-3 -->
-                </div><!-- end row -->
-               </div>
-            </li>
-            <li class="has-submenu">
-               <a class="dropdown-item dropdown-toggle" href="#"> Dropdown item 5 </a>
-               <div class="megasubmenu dropdown-menu">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat.
-               </div>
-            </li>
-            <li><a class="dropdown-item" href="#"> Dropdown item 6 </a></li>
-            </ul>
-          </li>
-          <li class="nav-item"> <a class="nav-link" href="#">Menu item </a> </li>
-          <li class="nav-item"><a class="nav-link" href="#"> About </a></li>
-          <li class="nav-item"><a class="nav-link" href="#"> Services </a></li>
-        </ul>
+       
         
         <ul class="navbar-nav ml-auto">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"> Mega submenu </a>
+              <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#"> Dropdown item 1 </a></li>
+              <li><a class="dropdown-item" href="#"> Dropdown item 2 </a></li>
+              <li><a class="dropdown-item" href="#"> Dropdown item 3 </a></li>
+              <li class="has-submenu">
+                 <a class="dropdown-item dropdown-toggle" href="#"> Dropdown item 4 </a>
+                 <div class="megasubmenu dropdown-menu">
+                 <div class="row">
+                    <div class="col-6">
+                        <h6 class="title">Title Menu One</h6>
+                        <ul class="list-unstyled">
+                          <li><a href="#">Custom Menu</a></li>
+                          <li><a href="#">Custom Menu</a></li>
+                          <li><a href="#">Custom Menu</a></li>
+                          <li><a href="#">Custom Menu</a></li>
+                          <li><a href="#">Custom Menu</a></li>
+                        </ul>
+                    </div><!-- end col-3 -->
+                    <div class="col-6">
+                      <h6 class="title">Title Menu Two</h6>
+                        <ul class="list-unstyled">
+                          <li><a href="#">Custom Menu</a></li>
+                          <li><a href="#">Custom Menu</a></li>
+                          <li><a href="#">Custom Menu</a></li>
+                          <li><a href="#">Custom Menu</a></li>
+                        </ul>
+                    </div><!-- end col-3 -->
+                  </div><!-- end row -->
+                 </div>
+              </li>
+              <li class="has-submenu">
+                 <a class="dropdown-item dropdown-toggle" href="#"> Dropdown item 5 </a>
+                 <div class="megasubmenu dropdown-menu">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat.
+                 </div>
+              </li>
+              <li><a class="dropdown-item" href="#"> Dropdown item 6 </a></li>
+              </ul>
+            </li>
+            <li class="nav-item"> <a class="nav-link" href="#">Menu item </a> </li>
+            <li class="nav-item"><a class="nav-link" href="#"> About </a></li>
+            <li class="nav-item"><a class="nav-link" href="#"> Services </a></li>
+          </ul>
           <li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
           <li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
           <li class="nav-item dropdown">
