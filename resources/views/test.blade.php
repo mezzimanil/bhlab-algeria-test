c07c3eae1aba632787c58b9d3537ccca4fcece65
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-09 12:32:01 +0100

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -18,10 +18,11 @@
 	
 
 		<nav class="megamenu fixed-top">
-			<a class="navbar-brand" href="{{ url('/') }}">
-				<img src="{{ asset('image/lol.png') }}" >
-			</a>
+			
 <ul class="megamenu-nav d-flex justify-content-center" role="menu">
+	<a class="navbar-brand" href="{{ url('/') }}">
+		<img src="{{ asset('image/lol.png') }}" >
+	</a>
 <li class="nav-item">
   <a class="nav-link" href="{{ url('/') }}">
 	  <i class="fa fa-home"></i>
