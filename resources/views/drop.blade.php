cd461d99c9f8e9ed8e4a2db4c14e724a50b26dea
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-10 17:17:59 +0100

    Update drop.blade.php

--- resources/views/drop.blade.php
+++ resources/views/drop.blade.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
   
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    
 
 
     <script src="{{ asset('js/modernizr.js') }}"></script> <!-- Modernizr -->
