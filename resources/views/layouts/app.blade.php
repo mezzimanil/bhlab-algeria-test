f0c85569315439757ddd17b19703e9b93039d873
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-10 11:30:26 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,8 @@
     <!-- Fonts -->
     
     
-
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
     <!-- Styles -->
  
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -55,7 +56,88 @@
 
 
       
-
+      .megamenu {
+    background: #e7e2e2;
+    z-index: 15;
+  }
+  .megamenu .megamenu-nav {
+    padding: 0;
+    margin: 0;
+  }
+  .megamenu .megamenu-nav .nav-item {
+    display: block;
+  }
+  .megamenu .megamenu-nav .nav-item.is-open > .megamenu-content {
+    visibility: visible;
+    opacity: 1;
+  }
+  .megamenu .megamenu-nav .nav-item.is-open > .nav-link {
+    background: rgb(0,168,186);
+    
+  }
+  .megamenu .megamenu-nav .nav-link {
+    color: rgb(36, 50, 136);
+    padding: 15px 30px;
+  }
+  .megamenu .megamenu-nav .nav-link:hover {
+    background: rgb(0,168,186);
+    color: #333;
+  }
+  .megamenu .megamenu-content {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 54px;
+    overflow: hidden;
+    visibility: hidden;
+    opacity: 0;
+    z-index: 14;
+    transition: all .3s ease-in-out;
+  }
+  .megamenu .megamenu-content .container {
+    padding: 45px 15px;
+  }
+  .megamenu .megamenu-content .subnav {
+    margin: 0;
+    padding: 0;
+  }
+  .megamenu .megamenu-content .subnav-item {
+    display: block;
+  }
+  .megamenu .megamenu-content .subnav-item .subnav-link {
+    padding: 10px 0;
+    display: block;
+  }
+  
+  .megamenu-background {
+    background: #fff;
+    position: absolute;
+    left: 0;
+    top: 54px;
+    right: 0;
+    height: 0;
+    transition: all .3s ease-in-out;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.15);
+    z-index: 13;
+  }
+  
+  .megamenu-dim {
+    background: rgba(0, 0, 0, 0.6);
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 54px;
+    visibility: hidden;
+    opacity: 0;
+    z-index: -12;
+    transition: all .3s ease-in-out;
+  }
+  body.megamenu-visible .megamenu-dim {
+    z-index: 12;
+    visibility: visible;
+    opacity: 1;
+  }
 
     
       </style>
@@ -64,8 +146,30 @@
     <div id="app">
    
 
-     @include('test');
-       
+     
+     <nav class="megamenu fixed-top bg-light">
+      <div class="container">
+          <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('image/lol.png') }}" >
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <!-- Left Side Of Navbar -->
+              <ul class="navbar-nav mr-auto">
+
+              </ul>
+
+              <!-- Right Side Of Navbar -->
+              <ul class="navbar-nav ml-auto megamenu-nav d-flex justify-content-center">
+                  <!-- Authentication Links -->
+                  
+              </ul>
+          </div>
+      </div>
+  </nav>
               
              
       
@@ -177,5 +281,102 @@
 
   <!-- Footer -->
 </body>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.hoverintent/1.9.0/jquery.hoverIntent.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js'></script>
+
+<script type="text/javascript">
+	/// some script
+	
+	// jquery ready start
+	const $megamenuParentListItem = $('.megamenu-nav > li.is-parent');
+
+const $megamenuBackground = $('#megamenu-background');
+
+const isTouch = 'ontouchstart' in window || !!navigator.msMaxTouchPoints;
+
+const handleMenuItemOpenState = elem => {
+  elem.addClass('is-open');
+  elem.find('a').first().attr('aria-expanded', true);
+};
+
+const handleMenuItemCloseState = elem => {
+  elem.removeClass('is-open');
+  elem.find('a').first().attr('aria-expanded', false);
+};
+
+const openMegamenu = (bgElem, heightVal) => {
+  $('body').addClass('megamenu-visible');
+  bgElem.height(heightVal);
+};
+
+const closeMegamenu = (bgElem, heightVal) => {
+  $('body').removeClass('megamenu-visible');
+  bgElem.height(heightVal);
+};
+
+const $megamenuContentElem = $('.megamenu-nav .megamenu-content');
+
+const getTallestMenuHeight = () => {
+  let maxHeight = 0;
+  $megamenuContentElem.each((index, item) => {
+    if ($(item).outerHeight() > maxHeight) {
+      maxHeight = $(item).outerHeight();
+    }
+  });
+  return maxHeight;
+};
+
+const debouncedClose = _.debounce(closeMegamenu, 400);
+const throttledContentHeightCount = _.throttle(getTallestMenuHeight, 100);
+
+let megamenuContentMaxHeight = 0;
+
+window.onresize = () => {
+  megamenuContentMaxHeight = throttledContentHeightCount();
+};
+
+$(() => {
+  megamenuContentMaxHeight = getTallestMenuHeight();
+
+  $megamenuParentListItem.each((index, item) => {
+    if (!isTouch) {
+      $(item).hoverIntent({
+        sensitivity: 10,
+        interval: 50,
+        over: () => {
+          debouncedClose.cancel();
+          $megamenuParentListItem.removeClass('is-open');
+          handleMenuItemOpenState($(item));
+          openMegamenu($megamenuBackground, megamenuContentMaxHeight);
+        },
+        out: () => {
+          handleMenuItemCloseState($(item));
+          debouncedClose($megamenuBackground, 0);
+        } });
+
+    }
+
+    $(item).find('a').first().on('click touch', () => {
+      if (!$(item).hasClass('is-open')) {
+        $megamenuParentListItem.removeClass('is-open');
+        handleMenuItemOpenState($(item));
+        openMegamenu($megamenuBackground, megamenuContentMaxHeight);
+      } else {
+        handleMenuItemCloseState($(item));
+        closeMegamenu($megamenuBackground, 0);
+      }
+    });
+  });
+
+  $('#megamenu-dim').on('click touch', e => {
+    if ($('body').hasClass('megamenu-visible')) {
+      e.preventDefault();
+      $megamenuParentListItem.removeClass('is-open');
+      closeMegamenu($megamenuBackground, 0);
+    }
+  });
+});
+	</script>
 
 </html>
