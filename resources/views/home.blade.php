cc1333d5164c80685812a975b3d470db3948b449
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-11 16:04:02 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,12 +48,10 @@
 <main role="main">
 	@include('teste')
 	<div class="header-intro parallax100 t-center p-t-135 p-b-158" style="background-color:rgb(36,50,136);">
-		<span class="tit2 p-l-15 p-r-15">
-			Discover
-		</span>
+		
 
 		<h3 class="tit4 t-center p-l-15 p-r-15 p-t-3">
-			our Activities
+		Equipement et réactif de laboratoires
 		</h3>
 	</div>
 </main>
