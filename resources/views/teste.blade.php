16587ec30636dfb8e1ab1dbd484559cd74020b2a
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-11 13:17:09 +0100

    Update teste.blade.php

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -250,7 +250,8 @@
       <div class="mask flex-center">
       
           <div class="row align-items-center">
-          <img src="{{ asset('image/parte.png') }}" class="mx-auto" alt="slide" style="height: 800px">
+            <div class="col-md-7 col-12 order-md-2 order-1"> <img src="{{ asset('image/parte.png') }}" class="mx-auto" alt="slide" style="height: 800px">
+            </div>
             <div class="col-md-5 col-12 order-md-1 order-2">
               <h4>Present your <br>
                 awesome product</h4>
