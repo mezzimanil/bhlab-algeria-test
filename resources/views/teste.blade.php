365c9758dfe8d941a3d2635f782c7aec9452dec2
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-11 12:09:10 +0100

    Update teste.blade.php

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -249,13 +249,13 @@
       <div class="mask flex-center">
         <div class="container">
           <div class="row align-items-center">
-            <div class="col-md-7 col-12 order-md-1 order-2">
+            <div class="col-md-5 col-12 order-md-1 order-2">
               <h4>Present your <br>
                 awesome product</h4>
               <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
                 necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
               <a href="#">BUY NOW</a> </div>
-            <div class="col-md-5 col-12 order-md-2 order-1"><img src="https://i.imgur.com/duWgXRs.png" class="mx-auto" alt="slide"></div>
+            <div class="col-md-7 col-12 order-md-2 order-1"><img src="{{ asset('image/part.JPG') }}" class="mx-auto" alt="slide"></div>
           </div>
         </div>
       </div>
@@ -264,13 +264,13 @@
       <div class="mask flex-center">
         <div class="container">
           <div class="row align-items-center">
-            <div class="col-md-7 col-12 order-md-1 order-2">
+            <div class="col-md-5 col-12 order-md-1 order-2">
               <h4>Present your <br>
                 awesome product</h4>
               <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
                 necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
               <a href="#">BUY NOW</a> </div>
-            <div class="col-md-5 col-12 order-md-2 order-1"><img src="https://i.imgur.com/DGkR4OQ.png" class="mx-auto" alt="slide"></div>
+            <div class="col-md-7 col-12 order-md-2 order-1"><img src="{{ asset('image/corojpg') }}" class="mx-auto" alt="slide"></div>
           </div>
         </div>
       </div>
