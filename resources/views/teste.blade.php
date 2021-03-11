d9b0c046a486377518a5007db7a9c4213bbb1cd2
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-11 11:57:54 +0100

    Update teste.blade.php

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -233,6 +233,7 @@
     <div class="carousel-item active">
       <div class="mask flex-center">
         <div class="container">
+            <img src="{{ asset('image/labo.jpg') }}" class="mx-auto" alt="slide">
           <div class="row align-items-center">
             <div class="col-md-7 col-12 order-md-1 order-2">
               <h4>Present your <br>
@@ -240,7 +241,7 @@
               <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
                 necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
               <a href="#">BUY NOW</a> </div>
-            <div class="col-md-5  order-md-2 order-1"><img src="{{ asset('image/labo.jpg') }}" class="mx-auto" alt="slide"></div>
+           
           </div>
         </div>
       </div>
