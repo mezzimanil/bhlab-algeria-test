49cf09ef8a753948074bc19e7471798bdf1ace51
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-11 14:04:53 +0100

    Update teste.blade.php

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -236,9 +236,9 @@
         
           <div class="row align-items-center">
             <div class="col-md-6 col-12 order-md-1 order-2 ">
-              <h4 class="ml-2">Present your <br>
+              <h4 class="ml-5">Present your <br>
                 awesome product</h4>
-              <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
+              <p class="ml-5">Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
                 necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
               <a href="#">BUY NOW</a> </div>
             <div class="col-md-6 col-12 order-md-2 order-1"><img src="{{ asset('image/labo.jpg') }}" class="mx-auto" alt="slide" style="height: 800px"> </div>
