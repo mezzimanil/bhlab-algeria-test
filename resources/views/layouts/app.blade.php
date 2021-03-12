b1d3361baa365ede85bbd1f0861d151c209b4128
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-12 15:28:16 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -124,7 +124,7 @@
 }
 .megamenu .megamenu-nav .subnav-item .dropdown-menu li .dropdown-menu li .dropdown-menu {
     position: absolute;
-    right: -100%;
+    left: 100%;
     top: 0;
     opacity: 0 !important;
     visibility: hidden !important;
