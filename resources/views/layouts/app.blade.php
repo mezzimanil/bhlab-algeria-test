88fdc5419c23fc5b01000eaf15bc074292b6b331
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-12 15:32:22 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,7 +78,7 @@
             box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.05);
     background: #0d1028;
     position: absolute;
-    top: 80px;
+    
     left: 100%;
     width: 250px;
     z-index: 99;
