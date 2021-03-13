cb819e4422be872cce121fba6104c9f3aa032969
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-13 17:07:39 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -579,7 +579,7 @@
                               </div>
                             </div>
                           </div>
-                          <div class="col-5">
+                          <div class="col-4">
                             <img src="{{ asset('image/pa.png') }}" class="img-fluid mb-3" alt="test image">
                             <p>Plus de 15 marques pour répondre aux différents besoins de nos partenaire.</p>
                             <a href="#">Read more <i class="fa fa-angle-double-right"></i></a>
