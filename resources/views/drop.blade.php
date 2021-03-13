a712ec5716f85e01b2a3dee2bf1debd21f739020
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-13 11:32:34 +0100

    Update drop.blade.php

--- resources/views/drop.blade.php
+++ resources/views/drop.blade.php
@@ -9,201 +9,42 @@
 <title>Demo - Bootstrap multilevel navbar responsive. html code example </title>
 
 <!-- jQuery -->
-<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-crossorigin="anonymous"></script>
-
-<!-- Bootstrap files (jQuery first, then Popper.js, then Bootstrap JS) -->
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" type="text/javascript"></script>
 
 
-<script type="text/javascript">
-/// some script
+<script src="{{ asset('js/app.js') }}"></script>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-// jquery ready start
-$(document).ready(function() {
-	// jQuery code
 
-	//////////////////////// Prevent closing from click inside dropdown
-    $(document).on('click', '.dropdown-menu', function (e) {
-      e.stopPropagation();
-    });
 
-    // make it as accordion for smaller screens
-    if ($(window).width() < 992) {
-	  	$('.dropdown-menu a').click(function(e){
-	  		e.preventDefault();
-	        if($(this).next('.submenu').length){
-	        	$(this).next('.submenu').toggle();
-	        }
-	        $('.dropdown').on('hide.bs.dropdown', function () {
-			   $(this).find('.submenu').hide();
-			})
-	  	});
-	}
-	
-}); // jquery end
-</script>
-
-<style type="text/css">
-	@media (min-width: 992px){
-		.dropdown-menu .dropdown-toggle:after{
-			border-top: .3em solid transparent;
-		    border-right: 0;
-		    border-bottom: .3em solid transparent;
-		    border-left: .3em solid;
-		}
-
-		.dropdown-menu .dropdown-menu{
-			margin-left:0; margin-right: 0;
-		}
-
-		.dropdown-menu li{
-			position: relative;
-		}
-		.nav-item .submenu{ 
-			display: none;
-			position: absolute;
-			left:100%; top:-7px;
-		}
-		.nav-item .submenu-left{ 
-			right:100%; left:auto;
-		}
-
-		.dropdown-menu > li:hover{ background-color: #f1f1f1 }
-		.dropdown-menu > li:hover > .submenu{
-			display: block;
-		}
-	}
-</style>
 </head>
 <body class="bg-light">
 
-<header class="section-header py-4">
-<div class="container">
-	<h2>Demo page</h2>
-</div>
-</header> <!-- section-header.// -->
-
-<!-- ========================= SECTION CONTENT ========================= -->
-
-<div class="container">
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-
-  <a class="navbar-brand" href="#">Brand</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="main_nav">
-
-<ul class="navbar-nav">
-	<li class="nav-item active"> <a class="nav-link" href="#">Home </a> </li>
-	<li class="nav-item"><a class="nav-link" href="#"> About </a></li>
-	<li class="nav-item dropdown">
-		<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">  Treeview menu  </a>
-	    <ul class="dropdown-menu">
-		  <li><a class="dropdown-item" href="#"> Dropdown item 1 </a></li>
-		  <li><a class="dropdown-item" href="#"> Dropdown item 2 &raquo </a>
-		  	 <ul class="submenu dropdown-menu">
-			    <li><a class="dropdown-item" href="">Submenu item 1</a></li>
-			    <li><a class="dropdown-item" href="">Submenu item 2</a></li>
-			    <li><a class="dropdown-item" href="">Submenu item 3 &raquo </a>
-			    	<ul class="submenu dropdown-menu">
-					    <li><a class="dropdown-item" href="">Multi level 1</a></li>
-					    <li><a class="dropdown-item" href="">Multi level 2</a></li>
-					</ul>
-			    </li>
-			    <li><a class="dropdown-item" href="">Submenu item 4</a></li>
-			    <li><a class="dropdown-item" href="">Submenu item 5</a></li>
+	<div class="dropdown">
+		<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" data-hover="dropdown">
+		 Dropdown <span class="caret"></span>
+		</button>
+		<ul class="dropdown-menu">
+		  <li><a href="#">Action</a></li>
+		  <li><a href="#">Another action</a></li>
+		  <li class="dropdown">
+			<a href="#">One more dropdown</a>
+			<ul class="dropdown-menu">
+			  <li><a href="#">Action</a></li>
+			  <li><a href="#">Another action</a></li>
+			  <li class="dropdown">
+				<a href="#">One more dropdown</a>
+				<ul class="dropdown-menu">
+				...
+				</ul>
+			  </li>
+			  <li><a href="#">Something else here</a></li>
+			  <li><a href="#">Separated link</a></li>
 			 </ul>
 		  </li>
-		  <li><a class="dropdown-item" href="#"> Dropdown item 3 </a></li>
-		  <li><a class="dropdown-item" href="#"> Dropdown item 4 </a>
-	    </ul>
-	</li>
-	<li class="nav-item dropdown">
-		<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">  More items  </a>
-	    <ul class="dropdown-menu">
-		  <li><a class="dropdown-item" href="#"> Dropdown item 1 </a></li>
-		  <li><a class="dropdown-item dropdown-toggle" href="#"> Dropdown item 2 </a>
-		  	 <ul class="submenu dropdown-menu">
-			    <li><a class="dropdown-item" href="">Submenu item 1</a></li>
-			    <li><a class="dropdown-item" href="">Submenu item 2</a></li>
-			    <li><a class="dropdown-item" href="">Submenu item 3</a></li>
-			 </ul>
-		  </li>
-		  <li><a class="dropdown-item dropdown-toggle" href="#"> Dropdown item 3 </a>
-		  	 <ul class="submenu dropdown-menu">
-			    <li><a class="dropdown-item" href="">Another submenu 1</a></li>
-			    <li><a class="dropdown-item" href="">Another submenu 2</a></li>
-			    <li><a class="dropdown-item" href="">Another submenu 3</a></li>
-			    <li><a class="dropdown-item" href="">Another submenu 4</a></li>
-			 </ul>
-		  </li>
-		  <li><a class="dropdown-item dropdown-toggle" href="#"> Dropdown item 4 </a>
-		  	 <ul class="submenu dropdown-menu">
-			    <li><a class="dropdown-item" href="">Another submenu 1</a></li>
-			    <li><a class="dropdown-item" href="">Another submenu 2</a></li>
-			    <li><a class="dropdown-item" href="">Another submenu 3</a></li>
-			    <li><a class="dropdown-item" href="">Another submenu 4</a></li>
-			 </ul>
-		  </li>
-		  <li><a class="dropdown-item" href="#"> Dropdown item 4 </a></li>
-		  <li><a class="dropdown-item" href="#"> Dropdown item 5 </a></li>
-	    </ul>
-	</li>
-</ul>
-
-<ul class="navbar-nav ml-auto">
-	<li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
-	<li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
-	<li class="nav-item dropdown">
-		<a class="nav-link  dropdown-toggle" href="#" data-toggle="dropdown"> Dropdown right </a>
-	    <ul class="dropdown-menu dropdown-menu-right">
-		  <li><a class="dropdown-item" href="#"> Dropdown item 1</a></li>
-		  <li><a class="dropdown-item" href="#"> Dropdown item 2 </a></li>
-		  <li><a class="dropdown-item dropdown-toggle" href="#"> Dropdown item 3 </a>
-		  	 <ul class="submenu submenu-left dropdown-menu">
-			    <li><a class="dropdown-item" href="">Submenu item 1</a></li>
-			    <li><a class="dropdown-item" href="">Submenu item 2</a></li>
-			    <li><a class="dropdown-item" href="">Submenu item 3</a></li>
-			    <li><a class="dropdown-item" href="">Submenu item 4</a></li>
-			 </ul>
-		  </li>
-		  <li><a class="dropdown-item" href="#"> Dropdown item 4 </a></li>
-	    </ul>
-	</li>
-
-</ul>
-
-  </div> <!-- navbar-collapse.// -->
-
-</nav>
-
-<section class="section-content py-5">
-
-		<h6>Demo view: Bootstrap multilevel dropdown menu </h6>
-        <p>For this demo page you should connect to the internet to receive files from CDN  like Bootstrap CSS, Bootstrap JS and jQuery. </p>
-		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-
-		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-
-		<a href="http://bootstrap-menu.com/multilevel.html" class="btn btn-warning">Back to tutorial or Download code</a>
-
-</section>
-
-</div><!-- container //  -->
+		  <li><a href="#">Something else here</a></li>
+		  <li><a href="#">Separated link</a></li>
+		</ul>
+	  </div>
 
 </body>
 </html>
