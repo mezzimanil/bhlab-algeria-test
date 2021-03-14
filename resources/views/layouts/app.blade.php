57c1f109ca178d11411070a6b493f731afa8613b
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-14 09:57:32 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -700,28 +700,7 @@
                     <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
                 </ul>
               </div>
-              <div class="col-md-2 col-sm-2">
-                <h5 style="color: azure">For individuals</h5>
-                <ul class="footer-links">
-                  <li><a href="">Signup</a></li>
-                  <li><a href="">login</a></li>
-                  <li><a href="">Explore</a></li>
-                  <li><a href="">Finder app</a></li>
-                  <li><a href="">Features</a></li>
-                  <li><a href="">Language settings</a></li>
-                </ul>
-              </div>
-              <div class="col-md-2 col-sm-2">
-                <h5 style="color: azure">For businesses</h5>
-                <ul class="footer-links">
-                  <li><a href="">Business signup</a></li>
-                  <li><a href="">Business login</a></li>
-                  <li><a href="">Benefits</a></li>
-                  <li><a href="">Resources</a></li>
-                  <li><a href="">Advertise</a></li>
-                  <li><a href="">Setup</a></li>
-                </ul>
-              </div>
+              
               <div class="col-md-2 col-sm-2">
                 <h5 style="color: azure">About</h5>
                 <ul class="footer-links">
