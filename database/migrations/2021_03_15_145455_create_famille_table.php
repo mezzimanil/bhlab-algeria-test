02c4245c30439b32b53bf1cbe99ea36b617c5459
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-15 15:55:10 +0100

    tables

---
+++ database/migrations/2021_03_15_145455_create_famille_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFamilleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('famille', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('famille');
+    }
+}
