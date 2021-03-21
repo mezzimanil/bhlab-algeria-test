12c39017c6e1cdcd6e4d06806cc80ce7fcf82d01
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-21 13:41:28 +0100

    Update four.blade.php

--- resources/views/four.blade.php
+++ resources/views/four.blade.php
@@ -85,7 +85,7 @@
             <div class="content-intro  p-t-77 p-b-133" style="background-color: rgb(241, 241, 241)">
         <div class="container">
             <div class="row">
-                <div class="col-lg-5">
+                <div class="col-lg-2">
                 <div  id="myBtnContainer">
                    
                     
@@ -105,7 +105,7 @@
                   
                 </div>
                 </div>
-                <div class="col-lg-5">
+                <div class="col-lg-9">
                 <div class="container">
                     <div class="row">
                       
