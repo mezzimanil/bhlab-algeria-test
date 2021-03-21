bad456c8b216b6d16ce1ab0efa6b2d272287a1fc
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-21 12:33:50 +0100

    Update four.blade.php

--- resources/views/four.blade.php
+++ resources/views/four.blade.php
@@ -1,18 +1,19 @@
-@extends('layouts.app')
-@section('content')
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/util.css') }}" rel="stylesheet">
-    </head>
-  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
-
-<style>
-
-
-.container {
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/util.css') }}" rel="stylesheet">
+</head>
+<body>
+    <style>
+          
+        .container {
   overflow: hidden;
 }
 
@@ -41,22 +42,8 @@
   background-color: #666;
   color: white;
 }
-
-/*
-*
-* ==========================================
-* FOR DEMO PURPOSES
-* ==========================================
-*
-*/
-
-body {
-    min-height: 100vh;
-}
-</style>
-
-
-<body>
+    </style>
+    
 
   
 
@@ -76,6 +63,8 @@ body {
         <!-- Marketing messaging and featurettes
         ================================================== -->
         <!-- Wrap the rest of the page in another container to center all the content. -->
+
+        
       
         <div class="container marketing ">
       
@@ -385,6 +374,7 @@ body {
         </section>
         </div>
     </main>
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -439,4 +429,3 @@ body {
         </script>
 </body>
 </html>
-@endsection
