36bcefe67f42f250ebe6ed27678a93720ca8e59a
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-22 14:19:12 +0100

    Update four.blade.php

--- resources/views/four.blade.php
+++ resources/views/four.blade.php
@@ -79,18 +79,18 @@
           <div  id="myBtnContainer">
                    
                     
-            <button class="btn  active " onclick="filterSelection('all')"> Show all</button>
-            <button class="btn " onclick="filterSelection('Immuno Analyses')"> Immuno Analyses</button>
-            <button class="btn " onclick="filterSelection('Chimie Clinique')"> Chimie Clinique</button>
-            <button class="btn " onclick="filterSelection('Immuno Hémathologie')"> Immuno Hémathologie</button>
-            <button class="btn " onclick="filterSelection('Auto Immunité')"> Auto Immunité</button>
-            <button class="btn " onclick="filterSelection('Hémostase')"> Hémostase</button>
-            <button class="btn " onclick="filterSelection('Acute Care')"> Acute Care (Gaz de sang)</button>
-            <button class="btn " onclick="filterSelection('Consommables')"> Consommables</button>
-            <button class="btn " onclick="filterSelection('Typage HLA')"> Typage HLA</button>
-            <button class="btn " onclick="filterSelection('Bio moléculaire')"> Bio moléculaire</button>
-            <button class="btn " onclick="filterSelection('Immuno chimie')"> Immuno chimie</button>
-            <button class="btn " onclick="filterSelection('Hémathologie')"> Hémathologie</button>
+            <button class="btn  active dropdown-item" onclick="filterSelection('all')"> Show all</button>
+            <button class="btn dropdown-item" onclick="filterSelection('Immuno Analyses')"> Immuno Analyses</button>
+            <button class="btn dropdown-item" onclick="filterSelection('Chimie Clinique')"> Chimie Clinique</button>
+            <button class="btn dropdown-item" onclick="filterSelection('Immuno Hémathologie')"> Immuno Hémathologie</button>
+            <button class="btn dropdown-item" onclick="filterSelection('Auto Immunité')"> Auto Immunité</button>
+            <button class="btn dropdown-item" onclick="filterSelection('Hémostase')"> Hémostase</button>
+            <button class="btn dropdown-item" onclick="filterSelection('Acute Care')"> Acute Care (Gaz de sang)</button>
+            <button class="btn dropdown-item" onclick="filterSelection('Consommables')"> Consommables</button>
+            <button class="btn dropdown-item" onclick="filterSelection('Typage HLA')"> Typage HLA</button>
+            <button class="btn dropdown-item" onclick="filterSelection('Bio moléculaire')"> Bio moléculaire</button>
+            <button class="btn dropdown-item" onclick="filterSelection('Immuno chimie')"> Immuno chimie</button>
+            <button class="btn dropdown-item" onclick="filterSelection('Hémathologie')"> Hémathologie</button>
 
         
       </div>
