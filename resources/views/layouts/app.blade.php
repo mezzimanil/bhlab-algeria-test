5f8c11f8ad92281bdbd1699945befcb0402b9cf7
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-22 15:57:16 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -350,21 +350,21 @@
                                
                                   <li class="subnav-item">
                                     <div class="contain"> 
-                                      <div class="filterDiv all"><a href="#">abbott</a></div>
-                                      <div class="filterDiv Immuno Analyses Chimie Clinique Immuno Hémathologie"><a href="#">ORTHO CLINICAL DIAGNOSTICS</a></div>
-                                      <div class="filterDiv Auto Immunité Hémostase Acute Care"><a href="#">Werfen</a></div>
-                                      <div class="filterDiv Immuno Analyses"><a href="#">DIASORIN </a></div>
-                                      <div class="filterDiv Consommables"><a href="#">CAPP APS</a></div>
-                                      <div class="filterDiv Typage HLA"><a href="#">LAGITRE</a></div>
-                                      <div class="filterDiv Immuno Analyses Bio moléculaire"><a href="#">ADALTIS</a></div>
-                                      <div class="filterDiv all"><a href="#">ARKRAY</a></div>
-                                      <div class="filterDiv Bio moléculaire"><a href="#">ENTROGEN USA</a></div>
-                                      <div class="filterDiv Immuno Hémathologie"><a href="#">MERCK SARL</a></div>
-                                      <div class="filterDiv Auto Immunité"><a href="#">D-TEK sa BELGIQUE</a></div>
-                                      <div class="filterDiv Immuno chimie"><a href="#">BINDING SITE</a></div>
-                                      <div class="filterDiv Bio moléculaire"><a href="#">SACACE BIOTECHNOLOGIES</a></div>
-                                      <div class="filterDiv all"><a href="#">BIO-RAD</a></div>
-                                      <div class="filterDiv Hémathologie"><a href="#"> HORIBA</a></div>
+                                      <div class="filterDiv all"><a href="https://www.abbott.com/">abbott</a></div>
+                                      <div class="filterDiv Immuno Analyses Chimie Clinique Immuno Hémathologie"><a href="https://www.orthoclinicaldiagnostics.com/">ORTHO CLINICAL DIAGNOSTICS</a></div>
+                                      <div class="filterDiv Auto Immunité Hémostase Acute Care"><a href=" https://www.werfen.com/">Werfen</a></div>
+                                      <div class="filterDiv Immuno Analyses"><a href="https://www.diasorin.com/">DIASORIN </a></div>
+                                      <div class="filterDiv Consommables"><a href=" https://www.capp.dk/">CAPP APS</a></div>
+                                      <div class="filterDiv Typage HLA"><a href="https://www.lagitre.com/">LAGITRE</a></div>
+                                      <div class="filterDiv Immuno Analyses Bio moléculaire"><a href="http://www.adaltis.net/">ADALTIS</a></div>
+                                      <div class="filterDiv all"><a href="http://www.arkray.co.jp/">ARKRAY</a></div>
+                                      <div class="filterDiv Bio moléculaire"><a href=" http://entrogen.com/web3/">ENTROGEN USA</a></div>
+                                      <div class="filterDiv Immuno Hémathologie"><a href="https://www.merckgroup.com/">MERCK SARL</a></div>
+                                      <div class="filterDiv Auto Immunité"><a href="http://www.d-tek.be/">D-TEK sa BELGIQUE</a></div>
+                                      <div class="filterDiv Immuno chimie"><a href="https://www.bindingsite.com/">BINDING SITE</a></div>
+                                      <div class="filterDiv Bio moléculaire"><a href="https://sacace.com/">SACACE BIOTECHNOLOGIES</a></div>
+                                      <div class="filterDiv all"><a href="https://www.bio-rad.com/">BIO-RAD</a></div>
+                                      <div class="filterDiv Hémathologie"><a href="https://www.horiba.com/"> HORIBA</a></div>
                                     </div>
                                   </li>
                                 </ul>
