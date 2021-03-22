11420d974ec4c241b0ade94d9cb55efbe8c9f83a
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-22 21:56:11 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
 	<!--     Fonts and icons     -->
 	
 	<!-- CSS Files -->
-	@section('content')
+	
 
 	
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
@@ -23,7 +23,7 @@
  
 
 
-
+@section('content')
 <style>
 /* GLOBAL STYLES
 -------------------------------------------------- */
