320dfa9d08c6cc7f3975b377d3e505d7ced4fed1
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-22 18:24:03 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -241,7 +241,7 @@
              
               <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                  <img src="{{ asset('image/lole.png') }}" >
+                   BHLab
                 </a>
                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
