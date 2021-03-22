19a0895239093db26a6dc5169d21530d994b12db
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-22 21:52:24 +0100

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.app')
 
 
+<!DOCTYPE html>
 
-
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/util.css') }}" rel="stylesheet">
+</head>
 <body>
+    
+
+
+
     @section('content')
     <div style="background: url({{ asset('image/white.jpg') }})" class="jumbotron bg-cover text-white">
         <div class="container py-5 text-center">
