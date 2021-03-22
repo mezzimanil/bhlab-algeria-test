11c3e9973853c2c849e92f5663b1fdae6e686637
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-22 22:24:45 +0100

    Update home2.blade.php

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -60,62 +60,59 @@
 
         <!-- Top content -->
         <div class="top-content">
-       		<div class="row no-gutters">
-       			<div class="col">
-       				<div id="carousel-example" class="carousel slide" data-ride="carousel">
-       					<ol class="carousel-indicators">
-       						<li data-target="#carousel-example" data-slide-to="0" class="active"></li>
-       						<li data-target="#carousel-example" data-slide-to="1"></li>
-       						<li data-target="#carousel-example" data-slide-to="2"></li>
-       					</ol>
-       					<div class="carousel-inner">
-       						<div class="carousel-item active">
-       							<img src="{{ asset('image/1.jpg') }}" class="d-block w-100" alt="img1">
+			<div class="row no-gutters">
+				<div class="col">
+					<div id="carousel-example" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+							<li data-target="#carousel-example" data-slide-to="1"></li>
+							<li data-target="#carousel-example" data-slide-to="2"></li>
+						</ol>
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img src="{{ asset('image/d2.jpg') }}" class="d-block w-100" alt="img1">
+							 <div class="carousel-caption">
+								 <h1 class="wow fadeInLeftBig">Plus de 12 ans d’expérience dans le domaine du diagnostic médical</h1>
+								 <div class="description wow fadeInUp">
+								
+								 </div>
+							 </div>
+							</div>
+							<div class="carousel-item">
+								<img src="{{ asset('image/2.jpg') }}" class="d-block w-100" alt="img2">
 								<div class="carousel-caption">
-									<h1 class="wow fadeInLeftBig">Carousel Template with Bootstrap 4</h1>
-									<div class="description wow fadeInUp">
-										<p>
-											This is a free Carousel template made with the Bootstrap 4 framework.
-											Check it out now. Download it, customize and use it as you like!
-										</p>
-									</div>
-								</div>
-       						</div>
-       						<div class="carousel-item">
-       							<img src="{{ asset('image/2.jpg') }}" class="d-block w-100" alt="img2">
-       							<div class="carousel-caption">
-									<h1 class="wow fadeInLeftBig">This is Slide 2 of our Carousel</h1>
-									<div class="description wow fadeInUp">
-										<p>
-											You can download this free template on <a href="https://azmind.com" target="_blank">AZMIND</a>.
-										</p>
-									</div>
-								</div>
-       						</div>
-       						<div class="carousel-item">
-       							<img src="{{ asset('image/3.jpg') }}" class="d-block w-100" alt="img3">
-       							<div class="carousel-caption">
-									<h1 class="wow fadeInLeftBig">This is Slide 3, the Last One</h1>
-									<div class="description wow fadeInUp">
-										<p>
-											After you download it, you can customize and use it as you like. That's great!
-										</p>
-									</div>
-								</div>
-       						</div>
-       					</div>
-						<a class="carousel-control-prev" href="#carousel-example" role="button" data-slide="prev">
-							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-							<span class="sr-only">Previous</span>
-						</a>
-						<a class="carousel-control-next" href="#carousel-example" role="button" data-slide="next">
-							<span class="carousel-control-next-icon" aria-hidden="true"></span>
-							<span class="sr-only">Next</span>
-						</a>
-       				</div>
-       			</div>
-       		</div>
-        </div>
+								 <h1 class="wow fadeInLeftBig">This is Slide 2 of our Carousel</h1>
+								 <div class="description wow fadeInUp">
+									 <p style="color: white">
+										 You can download this free template on <a href="https://azmind.com" target="_blank">AZMIND</a>.
+									 </p>
+								 </div>
+							 </div>
+							</div>
+							<div class="carousel-item">
+								<img src="{{ asset('image/dark.jpg') }}" class="d-block w-100" alt="img3">
+								<div class="carousel-caption">
+								 <h1 class="wow fadeInLeftBig">This is Slide 3, the Last One</h1>
+								 <div class="description wow fadeInUp">
+									 <p style="color: white">
+										 After you download it, you can customize and use it as you like. That's great!
+									 </p>
+								 </div>
+							 </div>
+							</div>
+						</div>
+					 <a class="carousel-control-prev" href="#carousel-example" role="button" data-slide="prev">
+						 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						 <span class="sr-only">Previous</span>
+					 </a>
+					 <a class="carousel-control-next" href="#carousel-example" role="button" data-slide="next">
+						 <span class="carousel-control-next-icon" aria-hidden="true"></span>
+						 <span class="sr-only">Next</span>
+					 </a>
+					</div>
+				</div>
+			</div>
+	 </div>
 
         <!-- Section 1 -->
         <div class="section-1-container section-container">
