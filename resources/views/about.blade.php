ed4db61fa2eac431f81af945f3927fa532c66707
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-23 15:08:43 +0100

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -23,12 +23,14 @@
 <h4> Historique :
     BH LAB est une entreprise organisée sous forme de société à responsabilité limitée, dont le capital social est de 700.000 000 DA.
     Elle a été créée en  2007 ,son fondateur est : BENHAMIDA Anas.
+    <br>
     BH LAB est spécialisée dans l’importation, la mise en place, et la distribution de réactifs in vitro et d’automates  dédiés aux laboratoires d’analyses médicales.
     
 </h4>
 <h4>
 Activités : BH LAB représente exclusivement différentes marques de renommée mondiale, tels que les laboratoires : Ortho Clinical Diagnostics, Werfen, Diasorin, Sacace Biotechnologies, Lagitre , Adaltis, Arkray , Entrogen, Merck, Binding Site et Horiba.
-
+</h4>
+<h4>
     La mission principale  que se donne BH LAB est la mise en place de solutions pour les laboratoires d’analyses médicales ( L.A.M), aussi bien dans le secteur privé que dans les hôpitaux ( CHU, EHS, secteurs sanitaires), d’en assurer un approvisionnement régulier en réactifs, ainsi qu’un service après- vente  de qualité, incluant un haut niveau de maintenance et une aide à l’application pour l’interprétation des résultats,  garantissant ainsi un bon fonctionnement des automates et donc des résultats d’une précision irréprochable.
     BH LAB accompagne les laboratoires et se veut partenaire avant d’être fournisseur. Pour cela elle offre différents types de solutions qui répondent aux différents besoins des laboratoires .
 </h4>
