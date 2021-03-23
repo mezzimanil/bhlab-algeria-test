61a250d6593dc78ccf48dc67c3d78a20fe5645cf
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-23 17:32:28 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
  
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
    
-
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
     <style type="text/css">
 
 
