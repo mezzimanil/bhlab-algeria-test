d9c258d78c5fc0149c856bf590e2c7157e393f2b
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-23 11:19:21 +0100

    sombre

---
+++ resources/views/support.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/util.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div style="background: url({{ asset('image/white.jpg') }})" class="jumbotron bg-cover text-white">
+        <div class="container py-5 text-center">
+            <h1 class="display-4 font-weight-bold">Notre service après-vente
+                est à votre disposition 7 jours / 7</h1>
+           
+         
+        </div>
+    </div>
+    
+</body>
+</html>
