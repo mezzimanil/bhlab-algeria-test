ac7a014e38d064f0d959f00e69a26b2e6dab6e4f
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-23 16:42:09 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,8 @@
   
     <!-- Scripts -->
     
-    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
     
     <script src="{{ asset('js/app.js') }}"></script>
     
