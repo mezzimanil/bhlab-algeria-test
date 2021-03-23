cfe58649a500645d4ccc89e4e42a3df2d97be729
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-23 16:49:18 +0100

    Update four.blade.php

--- resources/views/four.blade.php
+++ resources/views/four.blade.php
@@ -7,7 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
     
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <link href="{{ asset('css/util.css') }}" rel="stylesheet">
 </head>
