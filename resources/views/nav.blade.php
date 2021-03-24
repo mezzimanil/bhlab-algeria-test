dbcde477ac7e303abba9a58e98d1727388461b71
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-24 11:35:50 +0100

    Update nav.blade.php

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -9,85 +9,48 @@
 </head>
 <style>
 
+.navbar-collapse {
+    float: left;
+    width: 100%;
+    clear: both;
+}
 
-/* Medium devices (tablets, 768px and up) 
-The navbar toggle appears at this breakpoint */
-@media only screen and (min-width: 768px) {
-  .navbar-collapse2.collapse {
-    display: block !important;
-    height: auto !important;
-    padding-bottom: 0px;
-    overflow: visible !important;
-  }
+@media (min-width: 768px) {
+    .navbar-right {
+        margin-top: -50px;
+    }
 }
 
 </style>
 <body>
   
     
-    <div id="wrap">
-        <div class="navbar navbar-inverse navbar-static-top hr" style="background:black;">
-            <div class="container">
-        
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <div class="navbar-brand logo"></div>
-                </div>
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav" style="font-size:17px;">
-                        <li><a href="#">Item1</a></li>
-                        <li class="divider-vertical"></li>
-                        <li><a href="#about">Item2</a></li>
-                        <li class="divider-vertical"></li>
-                        <li><a href="#contact">Item3</a></li>
-                        <li class="divider-vertical"></li>
-                        <li><a href="#contact">Item4</a></li>
-                        <li class="divider-vertical"></li>
-                        <li><a href="#contact">Item</a></li>
-                        <li class="divider-vertical"></li>
-                        <li><a href="#contact">Item6</a></li>
-                        <li class="divider-vertical"></li>
-                        <li><a href="#contact">Item7</a></li>
-        
-                    </ul>
-        
-                </div>
-            </div>
+    <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Logo/Brand</a>
         </div>
-            <div class="navbar navbar-inverse navbar-static-top hr" style="background:#ececec; border-top:none; ">
-                <div class="container">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse2">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                    </div>
-                    <div class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav" style="font-size:17px;">
-                            <li><a href="#">it1d</a></li>
-                            <li class="divider-vertical"></li>
-                            <li><a href="#about">it2d</a></li>
-                            <li class="divider-vertical"></li>
-                            <li><a href="#contact">it3d</a></li>
-                            <li class="divider-vertical"></li>
-                            <li><a href="#contact">it4d</a></li>
-                            <li class="divider-vertical"></li>
-                            <li><a href="#contact">it5d</a></li>
-                            <li class="divider-vertical"></li>
-                            <li><a href="#contact">it6d</a></li>
-                            <li class="divider-vertical"></li>
-                            <li><a href="#contact">it7d</a></li>
-        
-                        </ul>
-                    </div>
-                </div>
-            </div>
-   
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#">LinkA</a></li>
+            <li><a href="#">LinkB</a></li>
+            <li><a href="#">LinkC</a></li>
+            <li><a href="#">LinkD</a></li>
+            <li><a href="#">LinkE</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Link1</a></li>
+            <li><a href="#">Link2</a></li>
+            <li><a href="#">Link3</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+      
      
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
